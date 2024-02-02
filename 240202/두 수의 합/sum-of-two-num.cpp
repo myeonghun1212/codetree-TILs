a3963078ecd07c2f802daf4934875028a6b95e94
Myoung Hun <57068120+myeonghun1212@users.noreply.github.com>
2024-02-02 23:31:07 +0900

    Title: 두 수의 합, Time: 98ms, Memory: 3MB, Status: Passed - CodeTree

--- 240202/두 수의 합/sum-of-two-num.cpp
+++ 240202/두 수의 합/sum-of-two-num.cpp
@@ -11,7 +11,7 @@ int main(){
     for(int i =0 ;i < n; i++){
         int t;
         cin >> t;
-        if(map.find(t) != map.end()){
+        if(map.find(t) == map.end()){
             map[t] = 1;
         }
         else{
@@ -21,7 +21,12 @@ int main(){
     
     for(auto& i : map){
         if(map.find(k - i.first) != map.end()){
-            res += i.second * map[k - i.first];
+            if(i.first * 2 == k){
+                res += i.second * (i.second - 1);
+            }
+            else{
+                res += i.second * map[k - i.first];
+            }
         }
     }
     cout << res / 2;

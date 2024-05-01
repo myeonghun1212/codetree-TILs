bb966a789acc1385fc5e50ee1c10e85fd492ffea
Myoung Hun <57068120+myeonghun1212@users.noreply.github.com>
2024-05-01 20:04:52 +0900

    Title: 작은 구슬의 이동, Time: 111ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240501/작은 구슬의 이동/small-marble-movement.py
+++ 240501/작은 구슬의 이동/small-marble-movement.py
@@ -1,39 +1,28 @@
-n,t = map(int, input().split())
+n, t = map(int, input().split())
 
-c, r, d = input().split()
+r, c, d = input().split()
+
 r = int(r)
 c = int(c)
 
+dc = [0,1,0,-1]
+dr = [1,-0,1,0]
 
-direction = {
-    'U': (0,-1),
-    'D':(0,1),
-    'R':(1,0),
-    'L':(-1,0)
-}
-
-reverse = {
-    'U': 'D',
-    'D' : 'U',
-    'R' : 'L',
-    'L' : 'R',
-}
-skip = False
-#print(r, c, d)
-for i in range(t+1):
-    if skip:
-        skip = False
-        continue
-    r, c = r+direction[d][0], c + direction[d][1]
-    
-    tr = max(r, 1)
-    tc = max(c, 1)
-    tr = min(tr, n)
-    tc = min(tc, n)
-    if r != tr or c != tc:
-        d = reverse[d]
-        skip = True
-    r = tr
-    c = tc
-    #print(r, c, d)
-print(c, r)
+if d == 'U':
+    dindex = 0
+elif d == 'R':
+    dindex = 1
+elif d == 'D':
+    dindex = 2
+elif d == 'L':
+    dindex = 3
+#print(r, c, dindex)
+for i in range(t):
+    r += dr[dindex]
+    c += dc[dindex]
+    #print(r, c, dindex)
+    if not(1 <= r <= n) or not(1 <= c <= n):
+        r -= dr[dindex]
+        c -= dc[dindex]
+        dindex = (dindex + 2) % 4
+print(r, c)

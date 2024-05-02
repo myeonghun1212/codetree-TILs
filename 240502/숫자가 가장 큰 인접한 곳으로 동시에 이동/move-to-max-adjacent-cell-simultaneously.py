3093a1fc978f72c3eed6b29df72a97fd041fd630
Myoung Hun <57068120+myeonghun1212@users.noreply.github.com>
2024-05-02 21:43:22 +0900

    Title: 숫자가 가장 큰 인접한 곳으로 동시에 이동, Time: 142ms, Memory: 25MB, Status: Wrong Answer - CodeTree

---
+++ 240502/숫자가 가장 큰 인접한 곳으로 동시에 이동/move-to-max-adjacent-cell-simultaneously.py
@@ -0,0 +1,37 @@
+def checkVaild(n, t):
+    return 0<= t < n
+
+n, m, time = map(int, input().split())
+
+li = []
+
+for i in range(n):
+    t = list(map(int, input().split()))
+    li.append(t)
+
+balls = []
+for i in range(m):
+    r, c = map(int, input().split())
+    balls.append((r,c))
+
+dx = [0,0,-1,1]
+dy = [1,-1,0,0]
+
+for i in range(time):
+    tempballs = []
+    for ball in balls:
+        m = -1
+        mv = -1
+        for j in range(4):
+            tx = ball[1] + dx[j]
+            ty = ball[0] + dy[j]
+            if checkVaild(n, tx) and checkVaild(n, ty) and li[ty][tx] > mv:
+                # 이동 
+                m = j
+                mv = li[ty][tx]
+        
+        tempballs.append((ball[0] + dy[m], ball[1] + dx[m]))
+    tempballs = list(set(tempballs))
+    balls = tempballs
+
+print(len(balls))
